4006f5e928453f3f25aa524f5c08a7f0709fcb9b
Kenzie Dev <Cookie.mac@outlook.com>
2025-08-27 00:36:27 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'QBCore Custody Script (UK Style)'
+version '1.0.0'
+
+shared_script '@qb-core/shared.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua', -- if you add DB saving later
+    'server.lua'
+}
